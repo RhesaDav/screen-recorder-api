ba34c6d7a35122a18793296d37a09996d68ecc0d
Rhesa Davinanto <Callista Alifia>
2024-07-06 09:35:01 +0700

    uploading to s3

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,10 @@ import { executablePath } from "puppeteer";
 import fs from "fs";
 import path from "path";
 import ffmpeg from "fluent-ffmpeg";
-import { path as ffmpegPath } from '@ffmpeg-installer/ffmpeg';
-ffmpeg.setFfmpegPath(ffmpegPath);
+import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
+import * as dotenv from "dotenv"
 
+dotenv.config()
 
 const app = express();
 const port = 3000;
@@ -17,6 +18,36 @@ let recorder: any;
 let directoryName: string = "";
 
 app.use(express.json());
+
+const s3Client = new S3Client({
+  region: "ap-southeast-1",
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY || "",
+    secretAccessKey: process.env.AWS_SECRET_KEY || "",
+  },
+});
+
+const bucketName = "vcall-storage";
+const folderName = "vcall-recording";
+
+async function uploadToS3(filePath: string, fileName: string): Promise<string> {
+  const fileContent = fs.readFileSync(filePath);
+  const params = {
+    Bucket: bucketName,
+    Key: `${folderName}/${fileName}`,
+    Body: fileContent,
+  };
+
+  try {
+    await s3Client.send(new PutObjectCommand(params));
+    const s3Url = `https://${bucketName}.s3.amazonaws.com/${folderName}/${fileName}`;
+    console.log(`File uploaded successfully. ${s3Url}`);
+    return s3Url;
+  } catch (err) {
+    console.error("Error uploading file:", err);
+    throw err;
+  }
+}
 
 const recordingsDir = path.join(__dirname, "..", "recordings");
 if (!fs.existsSync(recordingsDir)) {
@@ -30,7 +61,7 @@ app.get("/start", async (req, res) => {
 
   if (!url || !doctor || !patient) {
     return res.status(400).json({
-      message: "Missing url, doctor, or patient query parameter"
+      message: "Missing url, doctor, or patient query parameter",
     });
   }
 
@@ -46,7 +77,7 @@ app.get("/start", async (req, res) => {
         height: 1080,
       },
       args: [
-          // "--no-sandbox"
+        // "--no-sandbox"
         //   "--disable-setuid-sandbox",
         "--headless=new",
         // "--use-fake-ui-for-media-stream",
@@ -66,7 +97,7 @@ app.get("/start", async (req, res) => {
       audio: true,
       video: true,
       videoBitsPerSecond: 2500000,
-      audioBitsPerSecond: 2500000
+      audioBitsPerSecond: 2500000,
     });
 
     const now = new Date();
@@ -79,7 +110,9 @@ app.get("/start", async (req, res) => {
     }
 
     const fileName = `recording_${timestamp}`;
-    recorder = stream.pipe(fs.createWriteStream(path.join(outputPath, `${fileName}.webm`)));
+    recorder = stream.pipe(
+      fs.createWriteStream(path.join(outputPath, `${fileName}.webm`))
+    );
 
     console.log("Recording started. Directory name:", directoryName);
 
@@ -91,7 +124,12 @@ app.get("/start", async (req, res) => {
 });
 
 app.post("/stop", async (req, res) => {
-  console.log("Stop endpoint called. Recorder:", !!recorder, "Directory name:", directoryName);
+  console.log(
+    "Stop endpoint called. Recorder:",
+    !!recorder,
+    "Directory name:",
+    directoryName
+  );
 
   try {
     if (recorder && directoryName) {
@@ -100,10 +138,13 @@ app.post("/stop", async (req, res) => {
       await browser.close();
 
       const outputPath = path.join(recordingsDir, directoryName);
-      const fileName = fs.readdirSync(outputPath).find(file => file.endsWith(".webm"))?.replace(".webm", "");
+      const fileName = fs
+        .readdirSync(outputPath)
+        .find((file) => file.endsWith(".webm"))
+        ?.replace(".webm", "");
       const webmPath = path.join(outputPath, `${fileName}.webm`);
       const mp4Path = path.join(outputPath, `${fileName}.mp4`);
-      const wavPath = path.join(outputPath, `${fileName}.mp3`);
+      const wavPath = path.join(outputPath, `${fileName}.wav`);
 
       console.log("WebM file path:", webmPath);
 
@@ -113,32 +154,12 @@ app.post("/stop", async (req, res) => {
 
       console.log(`Converting ${webmPath} to MP4 and WAV`);
 
-      // await new Promise<void>((resolve, reject) => {
-      //   ffmpeg(webmPath)
-      //     .outputOptions("-c:v libx264")
-      //     .outputOptions("-crf 23")
-      //     .outputOptions("-c:a aac")
-      //     .outputOptions("-b:a 128k")
-      //     .output(mp4Path)
-      //     .on("end", () => resolve())
-      //     .on("error", (err) => reject(err))
-      //     .run();
-      // });
-
-      // await new Promise<void>((resolve, reject) => {
-      //   ffmpeg(webmPath)
-      //     .outputOptions("-acodec pcm_s16le")
-      //     .outputOptions("-ac 2")
-      //     .output(wavPath)
-      //     .on("end", () => resolve())
-      //     .on("error", (err) => reject(err))
-      //     .run();
-      // });
-
       await new Promise<void>((resolve, reject) => {
         ffmpeg(webmPath)
-          .outputOptions("-c:v copy")
+          .outputOptions("-c:v libx264")
+          .outputOptions("-crf 23")
           .outputOptions("-c:a aac")
+          .outputOptions("-b:a 128k")
           .output(mp4Path)
           .on("end", () => resolve())
           .on("error", (err) => reject(err))
@@ -147,9 +168,8 @@ app.post("/stop", async (req, res) => {
 
       await new Promise<void>((resolve, reject) => {
         ffmpeg(webmPath)
-          .outputOptions("-vn")
-          .outputOptions("-c:a libmp3lame")
-          .outputOptions("-q:a 2")
+          .outputOptions("-acodec pcm_s16le")
+          .outputOptions("-ac 2")
           .output(wavPath)
           .on("end", () => resolve())
           .on("error", (err) => reject(err))
@@ -158,17 +178,32 @@ app.post("/stop", async (req, res) => {
 
       console.log("Conversion completed");
 
+      const mp4FileName = `${fileName}.mp4`;
+      const mp4S3Url = await uploadToS3(mp4Path, mp4FileName);
+
+      const wavFileName = `${fileName}.wav`;
+      const wavS3Url = await uploadToS3(wavPath, wavFileName);
+
       recorder = null;
       directoryName = "";
-      fs.unlink(webmPath, () => {});  // Remove the input file after conversion
 
-      res.json({ message: "Recording stopped, saved, and converted to MP4 and WAV" });
+      res.json({
+        message: "Recording stopped, saved, and converted to MP4 and WAV",
+        mp4Url: mp4S3Url,
+        wavUrl: wavS3Url,
+      });
     } else {
-      res.status(400).json({ error: "No active recording found or directoryName not set" });
+      res
+        .status(400)
+        .json({ error: "No active recording found or directoryName not set" });
     }
   } catch (error: any) {
     console.error("Error stopping recording or converting:", error);
-    res.status(500).json({ error: `Failed to stop recording or convert files: ${error.message}` });
+    res
+      .status(500)
+      .json({
+        error: `Failed to stop recording or convert files: ${error.message}`,
+      });
   }
 });
 
