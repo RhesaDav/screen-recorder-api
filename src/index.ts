085059069fcc653a5f3c547513528ea9f2061b1a
Rhesa Davinanto <Callista Alifia>
2024-07-10 04:03:57 +0700

    date format with jkt timezone

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ import ffmpeg from "fluent-ffmpeg";
 import ffmpegStatic from "ffmpeg-static";
 import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
 import dotenv from "dotenv";
+import { formatInTimeZone } from 'date-fns-tz';
 
 dotenv.config();
 
@@ -81,14 +82,15 @@ async function uploadToS3(filePath: string, directoryName: string) {
 }
 
 function dateToString(date: Date): string {
-  const year = date.getFullYear();
-  const month = (date.getMonth() + 1).toString().padStart(2, "0");
-  const day = date.getDate().toString().padStart(2, "0");
-  const hours = date.getHours().toString().padStart(2, "0");
-  const minutes = date.getMinutes().toString().padStart(2, "0");
-  const seconds = date.getSeconds().toString().padStart(2, "0");
+  // const year = date.getFullYear();
+  // const month = (date.getMonth() + 1).toString().padStart(2, "0");
+  // const day = date.getDate().toString().padStart(2, "0");
+  // const hours = date.getHours().toString().padStart(2, "0");
+  // const minutes = date.getMinutes().toString().padStart(2, "0");
+  // const seconds = date.getSeconds().toString().padStart(2, "0");
 
-  return `${year}${month}${day}T${hours}${minutes}${seconds}`;
+  // return `${year}${month}${day}T${hours}${minutes}${seconds}`;
+  return formatInTimeZone(date, 'Asia/Jakarta', 'yyyyMMdd\'T\'HHmmss')
 }
 
 const recordingsDir = path.join(__dirname, "..", "recordings");
